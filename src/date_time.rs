f7f6907977a1673ae2aa7179ebbff93b50ea6950
AbdulRhman Alfaifi <a.alfaifi.14@gmail.com>
2025-07-12 05:25:19 +0300

    v2.1.4
    * Fixed a bug in `DosDateTime`. Due to `DosDateTime` having resolution of 2 seconds, in some cases the seconds field will be `60` which is not valid
    * Change `chrono` functions to `_opt` version. Due to the previous bug, using the old functions will result in a panic
    * Pump up deps

--- src/date_time.rs
+++ src/date_time.rs
@@ -28,14 +28,31 @@ impl DosDateTime {
     }
     /// Create DosDateTime struct from `u32`.
     pub fn from_u32(num: u32) -> Result<Self> {
+        let day = (num & 31) as u8;
+        let month = ((num >> 5) & 15) as u8;
+        let year = ((num >> 9) & 127) as u16 + 1980;
+        let mut seconds = (((num >> 16) & 31) * 2) as u8;
+        let minutes = ((num >> 21) & 63) as u8;
+        let hour = ((num >> 27) & 31) as u8;
+
+        if seconds > 59 {
+            seconds = 59;
+        }
+
         Ok(DosDateTime {
-            day: (num & 31) as u8,
-            month: ((num >> 5) & 15) as u8,
-            year: ((num >> 9) & 127) as u16 + 1980,
-            seconds: (((num >> 16) & 31) * 2) as u8,
-            minutes: ((num >> 21) & 63) as u8,
-            hour: ((num >> 27) & 31) as u8,
+            day,
+            month,
+            year,
+            hour,
+            minutes,
+            seconds,
         })
+    }
+}
+
+impl Default for DosDateTime {
+    fn default() -> Self {
+        DosDateTime::new(1980, 1, 1, 0, 0, 0)
     }
 }
 
@@ -44,11 +61,16 @@ impl Display for DosDateTime {
         let ntime =
             match NaiveDate::from_ymd_opt(self.year as i32, self.month as u32, self.day as u32) {
                 Some(date) => {
-                    date.and_hms(self.hour as u32, self.minutes as u32, self.seconds as u32)
+                    match date.and_hms_opt(self.hour as u32, self.minutes as u32, self.seconds as u32) {
+                        Some (time) => time,
+                        // Unwraping here is safe because the timestamp is always valid.
+                        None => NaiveDate::from_ymd_opt(1980, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                    }
                 }
-                None => NaiveDate::from_ymd(1980, 1, 1).and_hms(0, 0, 0),
+                // Unwraping here is safe because the timestamp is always valid.
+                None => NaiveDate::from_ymd_opt(1980, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
             };
-        let dos_date_time: DateTime<Utc> = DateTime::from_utc(ntime, Utc);
+        let dos_date_time: DateTime<Utc> = DateTime::from_naive_utc_and_offset(ntime, Utc);
         write!(f, "{}", dos_date_time.format("%Y-%m-%dT%H:%M:%SZ"))
     }
 }
@@ -76,8 +98,9 @@ pub struct FileTime(DateTime<Utc>);
 impl FileTime {
     /// Create FileTime struct from `u64`.
     pub fn new(timestamp: u64) -> Self {
-        FileTime(DateTime::from_utc(
-            NaiveDate::from_ymd(1601, 1, 1).and_hms_nano(0, 0, 0, 0)
+        // Unwraping here is safe because the timestamp is always valid.
+        FileTime(DateTime::from_naive_utc_and_offset(
+            NaiveDate::from_ymd_opt(1601, 1, 1).unwrap().and_hms_nano_opt(0, 0, 0, 0).unwrap()
                 + Duration::microseconds((timestamp / 10) as i64),
             Utc,
         ))
